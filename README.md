9721bf28828620b28a4d9a6ab4ccca4fb7b49a9d
Indraneel Chakraborty <64887729+ineelhere@users.noreply.github.com>
2024-04-05 01:54:06 +0530

    [ADD] very basic starter streamlit app

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # startlit
+
+### STARTER
+This is a VERY basic streamlit app - just to get you started with your project.
---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+# Display a title
+st.title('Hello, World! 🌎🚀')
+
+# Add a description with an inline comment
+st.write("This is my first app in Streamlit! 📝")  # Comment: Don't forget to smile
+
