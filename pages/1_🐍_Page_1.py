419766a5723a0a7368612eb7b17f396996d1d504
Indraneel Chakraborty <64887729+ineelhere@users.noreply.github.com>
2024-04-07 09:09:00 +0530

    [ADD] multipage app template

---
+++ pages/1_🐍_Page_1.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("First Sub-page")
+
+st.write("This is the first sub-page of your Streamlit app.")
+st.write("You can add more elements here in this page.\nRead more at https://docs.streamlit.io/d")
---
+++ pages/4_🐍_Page_4.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("Fourth Sub-page")
+
+st.write("This is the Fourth sub-page of your Streamlit app.")
+st.write("You can add more elements here in this page.\nYou can add more elements here in this page.\nRead more at https://docs.streamlit.io/d")
