7ecfc9b1a562b6373453c696400a3aca2d31c567
Indraneel Chakraborty <64887729+ineelhere@users.noreply.github.com>
2024-04-08 16:02:18 +0530

    [ADD] correct reference url

--- pages/2_Page_2.py
+++ pages/2_Page_2.py
@@ -3,4 +3,4 @@ import streamlit as st
 st.title("Second Sub-page")
 
 st.write("This is the second sub-page of your Streamlit app.")
-st.write("You can add more elements here in this page.\nRead more at https://docs.streamlit.io/d")
+st.write("You can add more elements here in this page.\nRead more at https://docs.streamlit.io/develop/concepts/multipage-apps/pages-directory")
--- pages/4_Page_4.py
+++ pages/4_Page_4.py
@@ -3,4 +3,4 @@ import streamlit as st
 st.title("Fourth Sub-page")
 
 st.write("This is the Fourth sub-page of your Streamlit app.")
-st.write("You can add more elements here in this page.\nYou can add more elements here in this page.\nRead more at https://docs.streamlit.io/d")
+st.write("You can add more elements here in this page.\nYou can add more elements here in this page.\nRead more at https://docs.streamlit.io/develop/concepts/multipage-apps/pages-directory")
