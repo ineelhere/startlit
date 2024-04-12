bd806a3b25386ee83d769142ac53d68b630e7c2e
Indraneel Chakraborty <64887729+ineelhere@users.noreply.github.com>
2024-04-12 19:51:30 +0530

    [ADD] basic chat app

---
+++ app.py
@@ -0,0 +1,54 @@
+import streamlit as st
+import random
+import time
+
+
+# Streamed response emulator
+def response_generator():
+    response = random.choice(
+        [
+            "Hello there! How can I assist you today?",
+            "Hi, human! Is there anything I can help you with?",
+            "Do you need help?",
+        ]
+    )
+    for word in response.split():
+        yield word + " "
+        time.sleep(0.05)
+
+
+st.title("A dummy chatbot")
+
+with st.sidebar:
+    st.header("Information 💬")
+    st.info('''
+        Welcome to the chat application! 👋\n
+        Type your message in the chat input box and hit Enter to chat. 💬\n
+        Your conversation history will be displayed below. 📜
+    ''')
+    st.markdown("Read more at https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps 📚")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+if prompt := st.chat_input("Write your message here"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        response = st.write_stream(response_generator())
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
+
+st.markdown('Get this app project in your workspace with the `basic_chat()` function of [StartLit](https://pypi.org/project/startlit/) package')
